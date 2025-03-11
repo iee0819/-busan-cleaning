4cd000870b80e3454fc337bccb10b163e9c44a76
eun hye <iee0819@naver.com>
2025-03-11 15:59:59 +0900

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,8 @@
+<ul>
+                        <li><a href="./sub01.php">깔끔이청소 소개</a></li>
+                        <li><a href="./sub02.php">아파트입주청소</a></li>
+                        <li><a href="./sub03.php">이사/상가청소</a></li>
+                        <li><a href="./sub04.php">사무실청소</a></li>
+                        <li><a href="./sub05.php">청소갤러리</a></li>
+                        <li><a href="./sub06.php">질문과답변</a></li>
+                    </ul>
---
+++ sub04.php
@@ -0,0 +1,69 @@
+<?
+$title = '사무실청소';
+$num = 4;
+?>
+<? include './head.php'; ?>
+            <article class="content">
+                <h2><?=$title;?><span>깔끔이청소</span></h2>
+                <div class="title">
+                    <div class="txt_box">
+                        <img src="./images/logo.png" alt="">
+                        <strong class="gr_txt"><?=$title;?></strong>
+                        <p>부산입주청소 이사청소 오피스텔청소 상가청소 전문업체 부산아파트입주청소 <?=$title;?></p>
+                    </div>
+                </div>
+                <div class="desc">
+                    <strong>똑소리나게 일잘하는 부산 청소업체 부산아파트입주청소 <?=$title;?></strong>
+                    <p>
+                        홈페이지를 찾아주시는 고객 여러분 감사합니다.<br>
+                        합리적인 견적으로 정말 똑소리나게 일잘하는 부산 청소업체 부산아파트입주청소 금맥청소입니다.
+                    </p>
+                    <p class="mt16 mb24">
+                        최상의 제품과 최고의 서비스를 제공하기 위해 고객께 최선을 다하는 부산아파트입주청소 금맥청소 되겠습니다.<br>
+                        감사합니다.
+                    </p>
+                    <div class="cm">대표 전정선.</div>
+                </div>
+                <table class="table mt40">
+                    <caption class="blind">표에 대한 설명</caption>
+                    <colgroup>
+                        <col style="width: 100px;">
+                        <col />
+                    </colgroup>
+                    <tbody>
+                        <tr>
+                            <td>회사명</td>
+                            <td>깔끔이청소</td>
+                        </tr>
+                        <tr>
+                            <td>주&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;소</td>
+                            <td>부산광역시 북구 만덕2로 14 상가동 제314호</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">연 락 처 : <a href="tel:051-331-3354"></a>051-331-3354 </td>
+                        </tr>
+                    </tbody>
+
+                </table>
+
+                <hr class="hr" />
+
+                <ul class="fx">
+                    <li><img src="./images/main_s011.jpg" alt=""></li>
+                    <li><img src="./images/main_s012.jpg" alt=""></li>
+                    <li><img src="./images/main_s013.jpg" alt=""></li>
+                </ul>
+
+                <hr class="hr" />
+
+                <div class="desc">
+                    <strong>청소내용</strong>
+                    <ul class="dot_list">
+                        <li>쓰레기수거후 진공청소</li>
+                        <li>베란다 샷시 및 유리창 청소 (건물바깥면 유리창은 제외)</li>
+                        <li>베란다 샷시 및 유리창 청소 (건물바깥면 유리창은 제외)베란다 샷시 및 유리창 청소 (건물바깥면 유리창은 제외)베란다 샷시 및 유리창 청소 (건물바깥면 유리창은
+                            제외)</li>
+                    </ul>
+                </div>
+            </article>
+    <? include './tail.php'; ?>
